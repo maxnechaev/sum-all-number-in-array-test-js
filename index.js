f82ed1c186a8a7f6d571aca3ec0060e6bf6038af
Maxim Nechaev <4max.nechaev@gmail.com>
2018-08-30 20:50:39 +0000

    Sum Array of Numbers using recursion

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+// jshint esversion: 6
+
+
 function sumItems(array) {
-  // Sum all the numbers in the array
+  if (Array.isArray(array)) {
+    return array.reduce((num, sum) => num + sumItems(sum), 0);
+  }
+  return array;
 }
 
 module.exports = sumItems;
+
+
+// .reduce((a, b) => a + b, 0)
